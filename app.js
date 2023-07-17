bf1806ca5cd32853aff84cea4d883b85869e55a9
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2023-07-17 14:08:47 +0700

    update file app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const FileUpload = require('express-fileupload');
-const session = require('express-session');
-require('dotenv').config();
-const bodyParser = require('body-parser');
+const path = require('path');
 const db = require('./config/db');
 const allRouter = require('./routes');
 
@@ -16,31 +14,9 @@ db.then(() => {
   console.log(err);
 });
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      secure: 'auto',
-    },
-  })
-);
 app.use(cors());
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: [
-//       'https://final-project-fe-mental-hack.vercel.app',
-//       'http://127.0.0.1:5173',
-//     ],
-//   })
-// );
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(FileUpload());
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.use(allRouter);
 
 app.listen(process.env.PORT, () => {
