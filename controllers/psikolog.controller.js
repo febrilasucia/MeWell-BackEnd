dc4f8dc360711ae828f18682835e5cfe787628e9
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2023-08-05 21:17:43 +0700

    getPsikolgbyId

--- controllers/psikolog.controller.js
+++ controllers/psikolog.controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const Psikolog = require("../models/psikolog");
+const { default: mongoose } = require("mongoose");
 
 module.exports = {
   getAllPsikologRegis: async (req, res) => {
@@ -42,6 +43,60 @@ module.exports = {
         },
       ]);
       res.json(psikologs);
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
+
+  getPsikologRegisterById: async (req, res) => {
+    const { psikologId } = req.params;
+    try {
+      const psikolog = await Psikolog.aggregate([
+        {
+          $match: { _id: mongoose.Types.ObjectId(psikologId) },
+        },
+        {
+          $lookup: {
+            from: "users",
+            localField: "user_id",
+            foreignField: "_id",
+            as: "user",
+          },
+        },
+        {
+          $unwind: "$user",
+        },
+        {
+          $project: {
+            _id: 0,
+            psikolog_id: "$_id",
+            status: 1,
+            ijazah: 1,
+            ktp: 1,
+            univ: 1,
+            created_at: 1,
+            updated_at: 1,
+            user: {
+              user_id: "$user._id",
+              name: "$user.name",
+              role: "$user.role",
+              gender: "$user.gender",
+              place_birth: "$user.place_birth",
+              date_birth: "$user.date_birth",
+              email: "$user.email",
+              is_verified: "$user.is_verified",
+              profile: "$user.profile",
+            },
+          },
+        },
+      ]);
+      console.log(psikolog);
+
+      if (psikolog.length === 0) {
+        return res.status(404).json({ message: "Psikolog not found" });
+      }
+
+      res.json(psikolog[0]);
     } catch (error) {
       res.status(500).json({ message: "Internal server error" });
     }
--- routes/psikolog.router.js
+++ routes/psikolog.router.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { verifyToken, authorizeRoles } = require("../middleware/authUser");
-const { getAllPsikologRegis } = require("../controllers/psikolog.controller");
+const { getAllPsikologRegis, getPsikologRegisterById } = require("../controllers/psikolog.controller");
 
+router.get("/registrasi/:psikologId", getPsikologRegisterById);
 router.get("/registrasi", getAllPsikologRegis);
-
 
 module.exports = router;
